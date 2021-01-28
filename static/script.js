02247b8a4a267648d04ba1ebc64cbd8e90e56dfc
Raveen Panditha <raveenpanditha8@gmail.com>
2021-01-28 23:02:30 +0530

    finished button program

--- static/script.js
+++ static/script.js
@@ -10,6 +10,7 @@ function ageInDays(){
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
     
+//*!DO NOT SCROLL..
 
 }
 
@@ -23,14 +24,21 @@ function reset(){
 function generatecat(){
     var image = document.createElement('img');
     var div=document.getElementById('flex-cat-gen');
+   
     image.src="static/cat.jpg"; 
     div.appendChild(image);
 
 }       
 
 function removecat(){
-    var div=document.getElementById('flex-cat-gen');
-    div.replaceWith();
+    var el=document.getElementById('flex-cat-gen');
+    var vl=document.getElementById('flex-cat-gen');
+    
+
+    // div.style.display="none";
+     el.parentNode.removeChild(el);
+     
+  
 
 }
 
@@ -116,3 +124,67 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
 //Challenge 4: Change the Color of all Buttons
 var all_buttons = document.getElementsByTagName('button');
 console.log(all_buttons);
+
+var copyAllButtons = [];
+for(let i=0; i<all_buttons.length; i++){
+    copyAllButtons.push(all_buttons[i].classList[1]);
+
+}
+console.log(copyAllButtons);
+
+function buttonColorChange(buttonThingy){
+    if(buttonThingy.value ==='red'){
+        buttonsRed();
+    }else if(buttonThingy.value ==='green'){
+        buttonsGreen();
+
+    }else if(buttonThingy.value ==='reset'){
+        buttonColorReset();
+
+    }else if(buttonThingy.value ==='random'){
+        randomColors();
+
+    }
+
+
+}
+
+function buttonsRed(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+
+    }
+
+}
+
+function buttonsGreen(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+
+    }
+
+}
+
+function buttonColorReset(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+
+    }
+
+}
+
+function randomColors(){
+    var choices = ['btn-primary','btn-danger','btn-success','btn-warning']
+
+    for(let i=0;i<all_buttons.length; i++){
+        let randomNumber = Math.floor(Math.random()*4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+        
+
+    }
+
+}
