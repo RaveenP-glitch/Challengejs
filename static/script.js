74e2a936af31ca710186906d788662e7f5d938b2
Raveen Panditha <raveenpanditha8@gmail.com>
2021-01-28 17:30:22 +0530

    editing style of container

---
+++ static/script.js
@@ -0,0 +1,114 @@
+//Challenge 1: Your Age in Days
+
+function ageInDays(){
+    var birthYear=prompt("What year you were born... Good friend?");
+    var ageInDayss=(2020-birthYear)*365;
+    console.log(ageInDays);
+    var h1=document.createElement('h1');
+    var textAnswer=document.createTextNode('You are '+ageInDayss+' days old');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    
+
+}
+
+//DOM = Document Object Module
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+
+}
+
+function generatecat(){
+    var image = document.createElement('img');
+    var div=document.getElementById('flex-cat-gen');
+    image.src="static/cat.jpg"; 
+    div.appendChild(image);
+
+}       
+
+function removecat(){
+    var div=document.getElementById('flex-cat-gen');
+    div.replaceWith();
+
+}
+
+//Challenge 3: Rock, Paper, Scissors
+function rpsGame(yourChoice){
+    console.log(yourChoice);
+    var humanChoice,botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt());
+    console.log('Computer choice:',botChoice);
+    results = decideWinner(humanChoice,botChoice);// [0,1] human lost|bit won
+    message = finalMessage(results); //{message: 'You won!', 'color': 'green'}
+    console.log(message);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+
+
+
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random()*3);
+    
+}
+
+function numberToChoice(number){
+    return['rock','paper','scissors'][number];
+
+}
+
+function decideWinner(yourChoice, computerChoice){
+    var rpsDatabase={
+        'rock':{'scissors':1,'rock':0.5,'paper':0},
+        'paper':{'rock':1,'paper':0.5,'scissor':0},
+        'scissors':{'paper':1,'scissors':0.5,'rock':0},
+    };
+
+var yourScore = rpsDatabase[yourChoice][computerChoice];
+var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+return[yourScore,computerScore];
+
+}
+
+function finalMessage([yourScore,computerScore]){
+    if(yourScore === 0){
+        return{'message': 'You lost!','color': 'red'};
+
+    }else if(yourScore === 0.5){
+        return{'message': 'You tied!','color':'yellow'};
+
+    }else{
+        return{'message': 'You won!','color':'green'};
+    }
+
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
+    var imagesDatabase={
+        'rock':document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissors':document.getElementById('scissors').src
+    }
+
+    //let's remove all the images
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv=document.createElement('div');
+    var botDiv =document.createElement('div');
+    var messageDiv=document.createElement('div');
+
+    humanDiv.innerHTML="<img src='" + imagesDatabase[humanImageChoice] +"' width=150 height=150 style='box-shadow: 0px 10px 50px rgba(118, 176, 214, 0.945);overflow: hidden;justify-content:space-around;'>"
+    messageDiv.innerHTML="<h1 style='color: "+finalMessage['color'] + "; font-size: 40px; padding: 30px;'>"+finalMessage['message'] + "</h1>"
+    botDiv.innerHTML="<img src='" + imagesDatabase[botImageChoice] +"' width=150 height=150 style='box-shadow: 0px 10px 50px rgba(219, 97, 81, 0.945);overflow: hidden;justify-content:space-around;'>"
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+    
+
+}
